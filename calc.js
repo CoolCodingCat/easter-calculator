f34e6386a15365b9a527ca9f4dde3f8fb24dde2c
Lisa Korrigane <lisa@korrigane.dev>
2020-04-08 14:20:35 +0200

    removed prompt

--- calc.js
+++ calc.js
@@ -1,38 +1,57 @@
-const A = prompt('De quelle année voulez-vous connaître la date de Pâques ? (max 2099)');
-const answer = document.querySelector('.answer');
 
-let R = A%4;
-let S = A%7;
-let T = A%19;
+const send = document.querySelector(".send");
+send.addEventListener('click', function(){
 
-let B = (19*T)+24;
-let M = B%30;
+        console.log("send has been clicked");
 
-let C = (2*R)+(4*S)+(6*M)+5
+        const inputForm = document.querySelector(".yearInput");
+        inputForm.classList.add("is-hidden");
+    
+        const A = document.querySelector("#year").value;
+        console.log(A)
+    
+        let R = A%4;
+        let S = A%7;
+        let T = A%19;
+    
+        let B = (19*T)+24;
+        let M = B%30;
+    
+        let C = (2*R)+(4*S)+(6*M)+5
+    
+        let N = C%7;
+    
+        let P = M+N;
+    
+        console.log(P);
+    
+    
+        const answer = document.querySelector('.answer');
+    
+        if (P<10){
+            let paques = P + 22;
+            //console.log(paques + 'mars');
+            const date = document.querySelector(".message-body");
+            const visible = document.querySelector(".answer")
+            date.innerText = `Pâques tombe le ${paques} mars en ${A}`;
+            visible.classList.remove("is-hidden");
+        } else {
+            let paques = P - 9;
+            //console.log(paques + 'avril');
+            const date = document.querySelector(".message-body");
+            const visible = document.querySelector(".answer")
+            date.innerText = `Pâques tombe le ${paques} avril en ${A}`;
+            visible.classList.remove("is-hidden");  
+        }
+    }
 
-let N = C%7;
+);
 
-let P = M+N;
 
-console.log(P);
 
-if (P<10){
-    let paques = P + 22;
-    //console.log(paques + 'mars');
-    const date = document.querySelector(".message-body");
-    const visible = document.querySelector(".answer")
-    date.innerText = `Pâques tombe le ${paques} mars en ${A}`;
-    visible.classList.remove("is-hidden");
-} else {
-    let paques = P - 9;
-    //console.log(paques + 'avril');
-    const date = document.querySelector(".message-body");
-    const visible = document.querySelector(".answer")
-    date.innerText = `Pâques tombe le ${paques} avril en ${A}`;
-    visible.classList.remove("is-hidden");  
-}
 
-const button = document.querySelector(".button");
+
+const button = document.querySelector(".reset");
 button.addEventListener('click', function(){
     window.location.reload(false);
 })
